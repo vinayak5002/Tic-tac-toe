647f3aeba3877e577c44b9bbda2e5d0ed623d5eb
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2021-08-31 22:50:41 +0530

    initialized 9 buttons

--- app/src/main/java/com/tic_tac_toe/app/GamePage.java
+++ app/src/main/java/com/tic_tac_toe/app/GamePage.java
@@ -4,10 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class GamePage extends AppCompatActivity {
+    int count = 0;
+    public void press(Button view) {
+        count ++;
+        if(count %2 == 0){
+            view.setText("X");
+        }
+        else{
+            view.setText("O");
+        }
 
-    
+        view.setEnabled(false);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
