9c927ba60257f70c1c75edb0c297ff82db9228ac
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2021-08-31 19:05:40 +0530

    project initialized

---
+++ app/src/main/java/com/tic_tac_toe/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tic_tac_toe.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
