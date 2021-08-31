582e5a1ef9baa14e0c36e74c7e090266a4df402c
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2021-08-31 19:12:45 +0530

    Login page added

--- app/src/main/java/com/tic_tac_toe/app/MainActivity.java
+++ app/src/main/java/com/tic_tac_toe/app/MainActivity.java
@@ -2,8 +2,14 @@ package com.tic_tac_toe.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void userNameSubmit(View view){
+        Log.i("Submit button", "userNameSubmit: ");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
