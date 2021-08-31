e87c80cd3adfab28612e0e275e2b82e33d860998
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2021-08-31 19:34:55 +0530

    added second page and pressind button will take you to it

---
+++ app/src/main/java/com/tic_tac_toe/app/GamePage.java
@@ -0,0 +1,17 @@
+package com.tic_tac_toe.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class GamePage extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_page);
+    }
+}
--- app/src/main/java/com/tic_tac_toe/app/MainActivity.java
+++ app/src/main/java/com/tic_tac_toe/app/MainActivity.java
@@ -1,9 +1,11 @@
 package com.tic_tac_toe.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,22 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Submit button", "userNameSubmit: ");
     }
 
+    Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.UserNameSubmit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchGame = new Intent(MainActivity.this, GamePage.class);
+                startActivity(launchGame);
+            }
+        });
+
     }
 }
