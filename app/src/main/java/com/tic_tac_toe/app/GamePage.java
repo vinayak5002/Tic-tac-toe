fb8b912a8ce21348ca0b8c948bfc1d51d5ef744c
Munagapati Abhishek <siva@Munagapatis-iMac.local>
2021-09-02 00:16:12 +0530

    Added the rematch button and Randomized the first players turn

--- app/src/main/java/com/tic_tac_toe/app/GamePage.java
+++ app/src/main/java/com/tic_tac_toe/app/GamePage.java
@@ -1,5 +1,6 @@
 package com.tic_tac_toe.app;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.HashMap;
+import java.util.Random;
 
 
 public class GamePage extends AppCompatActivity {
@@ -19,7 +21,7 @@ public class GamePage extends AppCompatActivity {
     int[][] board = { {-1,-1,-1}, {-1,-1,-1}, {-1,-1,-1} };
     HashMap<Integer , PositionOfBoard> positions = new HashMap<Integer, PositionOfBoard>();
 
-    int count = 0;
+    int count = (new Random().nextInt())%2;
     int a[][] = new int[3][3];
 
     TextView title;
@@ -72,6 +74,13 @@ public class GamePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_page);
+        title = findViewById(R.id.gamePage);
+        if(count%2 == 0){
+            title.setText("O's turn");
+        }
+        else{
+            title.setText("X's turn");
+        }
         positions.put(R.id._00, new PositionOfBoard(0, 0));
         positions.put(R.id._01, new PositionOfBoard(0, 1));
         positions.put(R.id._02, new PositionOfBoard(0, 2));
@@ -83,4 +92,8 @@ public class GamePage extends AppCompatActivity {
         positions.put(R.id._22, new PositionOfBoard(2, 2));
     }
 
+    public void rematch(View view) {
+        Intent launch = new Intent(GamePage.this, GamePage.class);
+        startActivity(launch);
+    }
 }
